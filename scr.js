b731f1642e860ee57304728bacae73c9f3946c1a
Zakkina-AmongUs <imoutofideas26@gmail.com>
2026-08-31 21:55:52 +0200

    aspect ratio fix

--- scr.js
+++ scr.js
@@ -4,3 +4,20 @@ function discord() {
 function youtube() {
     window.open("https://www.youtube.com/@KanaGMD", "_blank")
 }
+
+const d_w = 1920;
+const d_h = 1080;
+
+function resize() {
+    const scale = Math.min(
+        window.innerWidth / d_w,
+        window.innerHeight / d_h
+    );
+    document.documentElement.style.setProperty(
+        "--scale",
+        scale
+    );
+}
+
+window.addEventListener("resize", resize);
+resize();
